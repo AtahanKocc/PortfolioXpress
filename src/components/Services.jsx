51735fec8ba43bc8d83d65d1db430c54a1d0e1df
Atahan Koc <koccata@gmail.com>
2023-09-24 21:04:07 +0300

    fix it services section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,61 +1,53 @@
 import React from 'react'
-import Section from './common/Section'
+import Section from "./common/Section"
 
-import commerce from '../assets/e-commerace.png'
-import web from '../assets/web.png';
-import mobile from '../assets/mobile.png';
-import research from '../assets/research.png';
+import commerce from "../assets/commerce.png";
+import research from "../assets/research.png";
+import web from "../assets/web.png";
+import mobile from "../assets/mobile.png";
 
 const Services = () => {
-  const services = [
-   {
-     id: 1,
-     image: commerce,
-     title: 'E-Commerce',
-   },
-   {
-    id: 2,
-    image: web,
-    title: 'Web Development',
-   },
-   {
-    id:3,
-    image: mobile,
-    title: 'Mobile Development',
-   },
-   {
-    id:4,
-    image: research,
-    title: 'Research',
-   },
-];
 
+    const services = [
+        {
+            id:1,
+            image:commerce,
+            title:"Ecommerce Website",
+        },
+        {
+            id:2,
+            image:web,
+            title:"Web developer",
+        },
+        {
+            id:3,
+            image:mobile,
+            title:"Mobile Developer",
+        },
+        {
+            id:4,
+            image:research,
+            title:"Research and Development",
+        },
+    ];
 
-return (  
-  <Section 
-  title="Services " 
-  subtitle="Here are all the services that I provide to my clients. I hope I offer
-  the service that you ae looking for.">
+  return (
+    <Section title="Services ⚒️" subtitle="Here are all the services that I provide to my clients. If you don't find something that you are looking for than please send me an email"
+    >
 
-   <div className='grid gap-10 lg:grid-cols-2'> 
-      {services.map(({id, image, title}) => (   
-    <div 
-        key={id} 
-        className='flex flex-col items-center justify-center p-5 shadow-lg 
-        dark:shadow-gray-100 rounded-xl duration-300 ease-in-out hover:scale-110'
-        >
-           
-        <img 
-           src={image} 
-           alt={title} 
-           className='w-36 h-36 md:w-44 md:h-44'
-        />
-        <h3 className='mt-5 text-base'>{title}</h3>   
+        <div className='grid gap-10 lg:grid-cols-2'>
+            {services.map(({id, image, title}) => (
+                <div key={id} className='flex flex-col items-center justify-center p-5 shadow-lg dark:shadow-gray-100 rounded-xl duration-300 ease-in-out hover:scale-110
+                '
+                >
+                    <img src={image} alt={title} className='w-36 h-36 md:w-44 md:h-44 object-contain' />
+                    <h3 className='mt-5 text-base'>{title}</h3>
+                </div>
+))}
         </div>
-      ))}
-   </div>
-  </Section>
-  );
-};
 
-export default Services;
+    </Section>
+  )
+}
+
+export default Services
