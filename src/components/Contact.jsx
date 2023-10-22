3668585ea70e73220a16a5ef7e7028d47f8253f7
Atahan Koc <koccata@gmail.com>
2023-10-22 15:39:39 +0300

    added form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,17 +30,65 @@ const Contact = () => {
     <Section title="Contact 📞" 
     subtitle="If you have any questions or would like to work together, please feel free to contact me.">     
     
-    <div>
-        <div><img src={contact} alt=''></img></div>
+    <div className='flex flex-col items-center justify-center gap-8 text-center'>
+        <div><img src={contact} alt='contact info' className='w-32 h-32'></img></div>
         <div>
-            <p>
+            <p className='max-w-xs md:max-w-lg font-extralight'>
                 I'm open to talk regarding freelancing or full-time opportunities.
             </p>
         </div>
-        <div>
+        <div className='flex w-full items-center justify-evenly text-3xl'>
             {SOCIAL.map(({id, link, icon}) => ( 
-               <a href="">{icon}</a>
+               <a 
+               href={link} 
+               target="_blank" 
+               rel='noopener noreferrer'
+               className='duration-200 ease-in-out hover:text-rose-300'>
+                {icon}
+                </a>
             ))}
+        </div>
+
+        <div className='p-8 text-left w-full'>
+          <form action='' method='POST'>
+            <div className='gap-4 w-full'>
+              <div className='flex flex-col'> 
+                   <label className='capitalize text-sm py-2 font-extralight'>
+                    Name
+                   </label>
+                   <input type='text' name='name' className='border-2 rounded-lg p-3 flex focus:outline-none border-gray-400
+                   dark:text-white'></input>
+                </div>
+
+                <div className='flex flex-col'> 
+                   <label className='capitalize text-sm py-2 font-extralight'>
+                    Surname
+                   </label>
+                   <input type='text' name='surname' className='border-2 rounded-lg p-3 flex focus:outline-none border-gray-400
+                   dark:text-white'></input>
+                </div>
+
+
+                <div className='flex flex-col'> 
+                   <label className='capitalize text-sm py-2 font-extralight'>
+                    Email
+                   </label>
+                   <input type='text' name='email' className='border-2 rounded-lg p-3 flex focus:outline-none border-gray-400
+                   dark:text-white'></input>
+                </div>
+
+                <div className='flex flex-col'> 
+                   <label className='capitalize text-sm py-2 font-extralight'>
+                    Message
+                   </label>
+                   <textarea
+                    name="message"
+                    rows="6"
+                    className='border-2 rounded-lg p-3 flex focus:outline-none border-gray-400'
+                   ></textarea>
+                </div>
+            </div>
+          </form>
         </div>
     </div>
    
