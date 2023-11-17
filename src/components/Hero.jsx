c00e9ab2bc7ecede6f044869ffa4f6679d2e81cb
Atahan Koc <koccata@gmail.com>
2023-11-17 14:47:07 +0300

    uptated cv

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,7 +61,7 @@ const Hero = () => {
             <img src={avatar2} alt='avatar' 
             className='w-60 h-60 md:w-72 md:h-72 object-cover object-top bg-gradient-to-b
            '/>
-            <a href="/AtahanKoc_Cv.pdf" download={true} className="flex items-center
+            <a href="/jason_cv.pdf" download={true} className="flex items-center
             justify-center mt-10 bg-gradient-to-r from-rose-600 to-teal-500 text-white py-2 rounded-lg">Resume</a>
          </div>
 
